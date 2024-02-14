5475ad822bbc781d9b96f3d177bc7991f6adac3e
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2024-02-14 00:37:45 +0000

    Changed property in Entity

--- src/Barbarosoft.TreeHouse.Domain.Model/InstructorEntity.cs
+++ src/Barbarosoft.TreeHouse.Domain.Model/InstructorEntity.cs
@@ -4,5 +4,5 @@ public class InstructorEntity
 {
     public int Id { get; set; }
     public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
+    public string? LastName { get; set; }
 }
