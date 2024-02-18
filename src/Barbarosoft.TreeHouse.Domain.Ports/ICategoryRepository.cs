1135f85e05eb409a256b39e64ed9450359aec5b8
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2024-02-18 02:02:59 +0000

    CategoryRepo and tests

--- src/Barbarosoft.TreeHouse.Domain.Ports/ICategoryRepository.cs
+++ src/Barbarosoft.TreeHouse.Domain.Ports/ICategoryRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace Barbarosoft.TreeHouse.Domain.Ports;
+﻿using Barbarosoft.TreeHouse.Domain.Model;
+
+namespace Barbarosoft.TreeHouse.Domain.Ports;
 
 public interface ICategoryRepository
 {
+    IEnumerable<CategoryEntity> GetAll();
+    CategoryEntity GetById(int categoryId);
 }
---
+++ tests/Barbarosoft.TreeHouse.Repository.SqlRepository.Tests/CategoryRepositoryTests.cs
@@ -0,0 +1,75 @@
+using Barbarosoft.TreeHouse.Repository.SqlRepository.Contexts;
+using Barbarosoft.TreeHouse.Domain.Model;
+using NUnit.Framework;
+using NSubstitute;
+using Barbarosoft.TreeHouse.Domain.Ports;
+using Barbarosoft.TreeHouse.Repository.SqlRepository.Repositories;
+
+namespace Barbarosoft.TreeHouse.Repository.SqlRepository.Tests;
+
+[TestFixture(Category = "unit")]
+internal class CategoryRepositoryTests
+{
+    ICourseApplicationContext _courseApplicationContext;
+    ICategoryRepository _categoryRepository;
+
+    [SetUp]
+    public void BeforeEach()
+    {
+        DbContextHelper.ResetDatabase();
+        _courseApplicationContext = DbContextHelper.Context;
+        _categoryRepository = new CategoryRepository(_courseApplicationContext);
+    }
+
+    [Test]
+    public void ReturnsAllCategories()
+    {
+        // Arrange
+        DbContextHelper.Add(new CategoryEntity
+        {
+            CategoryId = 1,
+            Name = "programming"
+        });
+        DbContextHelper.Add(new CategoryEntity
+        {
+            CategoryId = 2,
+            Name = "English"
+        });
+
+        // Act
+        var categories = _categoryRepository.GetAll();
+
+        // Assert
+        Assert.That(categories.Count(), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void ReturnsCategoryById()
+    {
+        // Arrange
+        int id = 3;
+        var categoryName = "programming";
+        DbContextHelper.Add(new CategoryEntity
+        {
+            CategoryId = id,
+            Name = categoryName
+        });
+
+        // Act
+        var category = _categoryRepository.GetById(id);
+
+        // Assert
+        Assert.That(category.Name, Is.EqualTo(categoryName));
+    }
+
+    [Test]
+    public void ThrowsNullReferenceExceptionWhenCategoryIdDoesNotExist()
+    {
+        var categoryId = -1;
+        // Act
+        Assert.Throws<NullReferenceException>(() =>
+        {
+            _categoryRepository.GetById(categoryId);
+        });
+    }
+}
