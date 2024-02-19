4c1202bd80589f474b8b56424fe8bb050e20b889
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2024-02-19 00:22:19 +0000

    Instructor Repo Impl

--- src/Barbarosoft.TreeHouse.Domain.Ports/IInstructorRepository.cs
+++ src/Barbarosoft.TreeHouse.Domain.Ports/IInstructorRepository.cs
@@ -1,5 +1,9 @@
-﻿namespace Barbarosoft.TreeHouse.Domain.Ports;
+﻿using Barbarosoft.TreeHouse.Domain.Model;
+
+namespace Barbarosoft.TreeHouse.Domain.Ports;
 
 public interface IInstructorRepository
 {
+    Task<InstructorEntity[]> ListAsync();
+    Task<InstructorEntity> GetById(int instructorId);
 }
