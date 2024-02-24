2dd279b517e7947766f81c1bb718e3d88d255df5
barbarosyurttagul <barbaros.yurttagul@gmail.com>
2024-02-24 14:46:11 +0000

    Course Service Impl

---
+++ src/Barbarosoft.TreeHouse.Service/CourseService.cs
@@ -0,0 +1,23 @@
+using Barbarosoft.TreeHouse.Domain.Model;
+using Barbarosoft.TreeHouse.Domain.Ports;
+
+namespace Barbarosoft.TreeHouse.Service;
+
+public class CourseService : ICourseService
+{
+    ICourseRepository _courseRepository;
+    public CourseService(ICourseRepository courseRepository)
+    {
+        _courseRepository = courseRepository;
+    }
+
+    public Task<CourseEntity[]> GetAll()
+    {
+        return _courseRepository.GetAll();
+    }
+
+    public Task<CourseEntity[]> GetByCategoryId(int categoryId)
+    {
+        return _courseRepository.GetByCategoryId(categoryId);
+    }
+}
